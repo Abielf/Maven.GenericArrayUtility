898610d4b71b3478dd8938e9995ecdc34dc87be0
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-27 11:17:53 -0400

    done lab

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,63 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T [] array;
+    public ArrayUtility(T [] inputArray) {
+        array=inputArray;
+
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+         Integer count=0;
+        for(T i:array){
+            if (i==valueToEvaluate){count++;}
+        }
+        return count;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        T[] newArray= Arrays.copyOf(array, array.length-getNumberOfOccurrences(valueToRemove));
+        int count =0;
+        for(int i=0;i< array.length;i++){
+             if (array[i]!=valueToRemove){
+                 newArray[count]=array[i];
+                 count++;
+             }
+        }
+        return newArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+       Integer dupes= getNumberOfOccurrences(valueToEvaluate);
+       for (T i:arrayToMerge){
+           if(i==valueToEvaluate){dupes++;}
+       }
+       return dupes;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        T returnThis = null;
+        Integer freqency=0;
+        for (T i:array){
+            if (countDuplicatesInMerge(arrayToMerge, i)>freqency){
+                freqency=countDuplicatesInMerge(arrayToMerge,i);
+                returnThis=i;
+            }
+        }
+        for (T i:arrayToMerge){
+            if (countDuplicatesInMerge(arrayToMerge, i)>freqency){
+                freqency=countDuplicatesInMerge(arrayToMerge,i);
+                returnThis=i;
+            }
+        }
+        return returnThis;
+    }
+
 }
